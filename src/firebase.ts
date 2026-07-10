a742dff1aa1471cad1f9beb3761ae9b7bb547b72
leonardoragnarson-cmd <leonardoragnarson@gmail.com>
2026-07-10 20:24:29 -0300

    Add files via upload

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,23 +1,17 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { initializeApp } from 'firebase/app';
-import { initializeFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBgw-KojD27fVZDK7-qUoYk4fFxNHN72E0",
-  authDomain: "gen-lang-client-0406630236.firebaseapp.com",
-  projectId: "gen-lang-client-0406630236",
-  storageBucket: "gen-lang-client-0406630236.firebasestorage.app",
-  messagingSenderId: "995237030066",
-  appId: "1:995237030066:web:8f5443675cbc17fa2501ab"
+  apiKey: "AIzaSyDyPEHCQNR4Q7qloUBl3kj8kM_ZfB3s8do",
+  authDomain: "eventos-insanos.firebaseapp.com",
+  projectId: "eventos-insanos",
+  storageBucket: "eventos-insanos.firebasestorage.app",
+  messagingSenderId: "23320521930",
+  appId: "1:23320521930:web:edd403f19e0c43a9021833",
 };
 
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore with the custom databaseId from configuration
-export const db = initializeFirestore(app, {}, "ai-studio-ba83e9a8-fc8a-4076-a857-d1a1e83deb69");
 export const auth = getAuth(app);
+export const db = getFirestore(app);
